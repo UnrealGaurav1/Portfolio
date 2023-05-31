2106755c124ae19da1eebf6dc798b8d499ee5f5b
UnrealGaurav1 <126160754+UnrealGaurav1@users.noreply.github.com>
2023-05-31 06:12:48 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
     <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/About" element={<About />}></Route>
-        <Route path="/Skills" element={<Skills />}></Route>
-        <Route path="/Projects" element={<Projects />}></Route>
-        <Route path="/Contact" element={<Contact />}></Route>
-        <Route path="/About-ReadMore" element={<ReadMore />}></Route>
+        <Route path="About" element={<About />}></Route>
+        <Route path="Skills" element={<Skills />}></Route>
+        <Route path="Projects" element={<Projects />}></Route>
+        <Route path="Contact" element={<Contact />}></Route>
+        <Route path="About-ReadMore" element={<ReadMore />}></Route>
       </Routes>
       <Footer />
     </Router> 
