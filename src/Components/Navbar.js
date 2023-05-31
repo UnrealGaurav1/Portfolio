965a6fe0807144d13f1fee3c654019eea9404d87
UnrealGaurav1 <126160754+UnrealGaurav1@users.noreply.github.com>
2023-05-31 06:51:11 -0800

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,10 +9,10 @@ const Navbar = () => {
         <span id="my-symbol">GAURAV</span>
       <div className="links">
        <Link to="/" className="link" id="home">Home</Link>
-        <Link to="/About" id="about" className="link">About</Link>
-        <Link to="/Skills" id="skills" className="link">Skills</Link>
-        <Link to="/Projects " id="contact" className="link">Projects</Link>
-        <Link to="/Contact" id="contact" className="link">Contact</Link>
+        <Link to="About" id="about" className="link">About</Link>
+        <Link to="Skills" id="skills" className="link">Skills</Link>
+        <Link to="Projects " id="contact" className="link">Projects</Link>
+        <Link to="Contact" id="contact" className="link">Contact</Link>
       </div>  
     </div>
   )
